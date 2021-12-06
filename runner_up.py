2834ddf60ed86327e21b3217e7446c4be29afb0b
Paun Adrian <adipaun97@gmail.com>
2021-12-06 19:46:35 +0200

    Add files via upload
    Find the runner-up score from a random generated array of integers.

---
+++ runner_up.py
@@ -0,0 +1,23 @@
+''' Find the runner-up score from an array '''
+
+import random
+
+def runner_up(array):
+	maxscore = 0
+	runner_up = 0
+	for i in array:
+		if i > maxscore:
+			maxscore, runner_up = i, maxscore
+		elif maxscore > i and runner_up < i:
+			runner_up = i
+	print("The max score is: " + str(maxscore))
+	print("The runner_up is: " + str(runner_up))
+
+print("Input the number of array elements: ")
+n = int(input())
+
+print("Generating an array...")
+array = random.sample(range(1,10), n)
+print("Array generated : " + str(array))
+
+runner_up(array)
